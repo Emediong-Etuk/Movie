4205c823acc2fa40e626ec8afeacdb936aa39887
Emediong-Etuk <emedionggregory12@gmail.com>
2025-02-21 22:58:41 +0100

    Authorization ( Admin and User)

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,23 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MovieController;
-use App\Http\Controllers\Auth\SignupController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\SignupController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\VerifyController;
+use App\Http\Middleware\EnsureUserIsAdmin;
+use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/movie', [MovieController::class, 'index'])->name('movie.index');
-Route::get('/verifyaccount', [MovieController::class, 'verifyaccount'])->name('verifyaccount');
-Route::post('/verifyotp', [MovieController::class, 'useractivation'])->name('verifyotp');
+Route::get('/admin/movie',[AdminController::class,'index'])->name('admin.index')->middleware(EnsureUserIsAdmin::class);
+Route::get('/verifyaccount', [VerifyController::class, 'verifyaccount'])->name('verifyaccount');
+Route::post('/verifyotp', [VerifyController::class, 'useractivation'])->name('verifyotp');
 Route::post('/movie/signup',[SignupController::class,'signup'])->name('movie.signup');
 Route::post('/movie/login',[LoginController::class,'login'])->name('movie.login');
 Route::get('/movie/loginpage',[LoginController::class,'loginpage'])->name('movie.loginpage');
